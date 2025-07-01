f68c4d5ad20a0ff7d16663caacbe334f798f3f39
pottervilla <115929350+pottervilla@users.noreply.github.com>
2025-07-01 07:10:32 +0530

    Delete models directory

--- models/Vendor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const VendorSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, required: true, unique: true },
-  password: String
-});
-
-module.exports = mongoose.model('Vendor', VendorSchema);
